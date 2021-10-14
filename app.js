db4eaf2d634197ada2e613226d7263426b5e9647
Aakash Prajapati <Aakash.Prajapati@MyGeorgian.ca>
2021-10-14 02:43:04 -0400

    New portfolio

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
 const express = require('express');
 const app = express();
-const exhbs = require('express-handlebars');
-
-
-app.use(express.static(__dirname + "/public"));
-app.use(require('./route'))
-
-app.set("view engine","handlebars");
-app.engine('handlebars',exhbs({
-
-    defaultLayout: 'index',
-    layoutsDir: __dirname + '/views/layout',
-    partialsDir: __dirname + '/views/partials'
-}));
-
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT);
-console.log(`listening ${PORT}`);
-
+const handlebar = require('express-handlebars');
